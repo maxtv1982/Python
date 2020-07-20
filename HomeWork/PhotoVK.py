c57ee1cfa2132e1b02a3a15ede26c4829589c448
maxtv1982 <maxtv@mail.ru>
2020-07-20 23:43:37 +0400

    редакция файла PhotoVK v1.1

--- HomeWork/PhotoVK.py
+++ HomeWork/PhotoVK.py
@@ -1,11 +1,9 @@
-import requests
+import os
+import sys
 import time
+import requests
 from tqdm import tqdm
-from pprint import pprint
 
-with open('C:\key.txt') as f:
-    token_vk = f.readline().rstrip()
-    token_ya = f.readline().rstrip()
 
 class UserVK:
 
@@ -19,34 +17,37 @@ class UserVK:
         response = requests.get('https://api.vk.com/method/photos.get',
                                 params={'access_token': token_vk, 'owner_id': self.UserID, 'album_id': album,
                                         'extended': 1, 'photo_sizes': 1, 'count': count, 'rev': 1, 'v': 5.21})
-        # собираем информацию по каждой фотографии и загружаем на компьютер
-        print(' <<< загружаем фотографии с профиля(стены) пользователя vk >>> ')
-        for photo in response.json()['response']['items']:
-            # прогресс-бар для каждого файла
-            for i in tqdm(range(100)):
-                time.sleep(0.01)
+        if response.status_code == 200 and 'error' not in response.json().keys():
+            # собираем информацию по каждой фотографии и загружаем на компьютер
+            print(' <<< загружаем фотографии с профиля(стены) пользователя vk >>> ')
+            for photo in response.json()['response']['items']:
 
-            download_photo = ''
-            type_size = 'a'
-            # ищем ссылку на фотографию с максимальным размером
-            for size in photo['sizes']:
-                if size['type'] > type_size:
-                    type_size = size['type']
-                    download_photo = size['src']
-            # создаём словарь с иформацией о фотографии
-            photo_info = {'file_name': photo['likes']['count'], 'size': type_size}
-            # если количество лайков одинаково, то добавить дату загрузки
-            for photos in self.photo_base:
-                if photo_info['file_name'] == photos['file_name']:
+                download_photo = ''
+                type_size = 'a'
+                # ищем ссылку на фотографию с максимальным размером
+                for size in photo['sizes']:
+                    if size['type'] > type_size:
+                        type_size = size['type']
+                        download_photo = size['src']
+                # создаём словарь с иформацией о фотографии
+                photo_info = {'file_name': photo['likes']['count'], 'size': type_size}
+                # если количество лайков одинаково, то добавить дату загрузки
+                if photo['likes']['count'] in (photos['file_name'] for photos in self.photo_base):
                     photo_info['file_name'] = f"{photo['likes']['count']}_{photo['date']}"
-            # добаляем словарь с иформацией о фотографии в базу фотографий
-            self.photo_base.append(photo_info)
-            # загружаем фотографию на компьютер
-            download = requests.get(download_photo)
-            with open(f"C:\Photo_VK\{photo_info['file_name']}.jpg", 'wb') as f:
-                f.write(download.content)
+                # добаляем словарь с иформацией о фотографии в базу фотографий
+                self.photo_base.append(photo_info)
+                # загружаем фотографию на компьютер
+                download = requests.get(download_photo)
+                # прогресс-бар для каждого файла
+                for i in tqdm(range(1)):
+                    time.sleep(0.005)
 
-        print(f'список загруженных файлов - {self.photo_base}')
+                name_photo = f"{photo_info['file_name']}.jpg"
+                with open(os.path.join("C:\Photo_VK", name_photo), 'wb') as f:
+                    f.write(download.content)
+            print(f'список загруженных файлов - {self.photo_base}')
+        else:
+            sys.exit('Некорректные данные пользователя VK')
 
     def upload_photo_Yandex(self, key):
         """ отправляем фотографий с профиля(стены) пользователя vk с компьютера на Яндекс.диск """
@@ -60,13 +61,10 @@ class UserVK:
 
         print(' <<< отправляем фотографии на Яндекс.диск >>>')
         for photo in self.photo_base:
-            # прогресс-бар для каждого файла
-            for i in tqdm(range(100)):
-                time.sleep(0.01)
 
             photo_name = f"{photo['file_name']}.jpg"
             # открываем загружаемый файл
-            with open(f'C:\Photo_VK\{photo_name}', 'rb') as new_file:
+            with open(os.path.join("C:\Photo_VK", photo_name), 'rb') as new_file:
                 my_file = new_file.read()
 
             # получаем загрузочную ссылку
@@ -76,23 +74,33 @@ class UserVK:
                 href = url_upload.json()['href']
                 # используя ссылку загружаем файл на диск
                 upload_file = requests.put(href, data=my_file)
+                # прогресс-бар для каждого файла
+                for i in tqdm(range(1)):
+                    time.sleep(0.005)
+
                 if upload_file.status_code == 201:
                     print('Success!')
                 else:
-                    print('Error')
+                    print('Ошибка загрузки')
             else:
-                print('Error')
+                print('Ошибка запроса, возможно файл с таким именем уже существует')
 
 
+if __name__ == '__main__':
+    # with open('C:\key.txt') as f:
+    #     token_vk = f.readline().rstrip()
+    #     token_ya = f.readline().rstrip()
+    #
+    # id_user = 552934290
+    # album = 'profile'
+    # count = 3
 
+    id_user = input('Введите id пользователя: ')
+    album = input('Из какого альбома нужно сохранить фотографии (wall/profile) : ')
+    count = input('Сколько фотографий сохранить: ')
+    token_vk = input('Введите токен для VK api: ')
+    token_ya = input('Введите токен с Полигона Яндекс.Диска: ')
 
-
-
-id_user = 552934290  # input('Введите id пользователя: ')
-album = 'profile'  # input('Из какого альбома нужно сохранить фотографии (wall/profile) : ')
-count = 3  # int(Сколько Фотографий сохранить: ')
-# token_ya = input('Введите токен с Полигона Яндекс.Диска: ')
-
-User = UserVK(id_user)
-User.get_photo_album(album, count)
-User.upload_photo_Yandex(token_ya)
+    User = UserVK(id_user)
+    User.get_photo_album(album, count)
+    User.upload_photo_Yandex(token_ya)
