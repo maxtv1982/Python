c2a7a15b485daa7b685de483bf1488c451627b81
maxtv1982 <maxtv@mail.ru>
2020-07-27 21:16:50 +0400

    обновленная версия photoVK

--- HomeWork/PhotoVK.py
+++ HomeWork/PhotoVK.py
@@ -46,6 +46,9 @@ class UserVK:
         else:
             sys.exit('Некорректные данные пользователя VK')
 
+
+class YandexAPIClient(UserVK):
+
     def upload_photo_Yandex(self, key_ya):
         """ отправляем фотографий с профиля(стены) пользователя vk с компьютера на Яндекс.диск """
 
@@ -78,12 +81,15 @@ class UserVK:
             else:
                 print('Ошибка запроса, возможно файл с таким именем уже существует')
 
+
+class GoogleAPIClient(UserVK):
+
     def upload_photo_Google(self, folder_id, service_key):
         """ отправляем фотографий с профиля(стены) пользователя vk с компьютера на Google.диск """
         # Указываем Scopes. Scopes — это перечень возможностей, которыми будет обладать сервис, созданный в скрипте.
         scopes = ['https://www.googleapis.com/auth/drive']
         # указываем путь к файлу с ключами сервисного аккаунта
-        service_account_file = os.path.join("..", service_key)
+        service_account_file = os.path.join("..", "..", service_key)
         # Создаем credentials (учетные данные), указав путь к сервисному аккаунту, а также заданные Scopes.
         credentials = service_account.Credentials.from_service_account_file(
             service_account_file, scopes=scopes)
@@ -94,48 +100,59 @@ class UserVK:
         name = 'Photo_VK'
         file_metadata = {'name': name, 'mimeType': 'application/vnd.google-apps.folder', 'parents': [folder_id]}
         result = service.files().create(body=file_metadata, fields='id').execute()
-        folder_id_VK = result['id']
+        folder_id_vk = result['id']
         # загрузка файла в папку
+        print(' <<< отправляем фотографии на Google.диск >>>')
         for photo in self.photo_base:
             photo_name = f"{photo['file_name']}.jpg"
             file_path = os.path.join("..", "Photo_VK", photo_name)
-            file_metadata = {'name': photo_name, 'parents': [folder_id_VK]}
+            file_metadata = {'name': photo_name, 'parents': [folder_id_vk]}
             media = MediaFileUpload(file_path, resumable=True)
             r = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
             print(f'- фотография {photo_name} c id - {r["id"]} загружена на Google.диск -')
 
 
 if __name__ == '__main__':
-    # with open(os.path.join("..", "key.txt")) as f:
-    #     token_vk = f.readline().rstrip()
-    #     token_ya = f.readline().rstrip()
-    #
-    # id_user = 552934290
-    # album = 'profile'
-    # count = 3
-    # folder_parent = '17kd8jJyLUZuNi2Br4TXWyxarkXZdjFiX'
-    # key = "river-runner-283818-aa3dd09a7020.json"
+    with open(os.path.join("..", "..", "key.txt")) as f:
+        token_vk = f.readline().rstrip()
+        token_ya = f.readline().rstrip()
 
-    id_user = input('Введите id пользователя: ')
-    album = input('Из какого альбома нужно сохранить фотографии (wall/profile) : ')
-    count = input('Сколько фотографий сохранить: ')
-    token_vk = input('Введите токен для VK api: ')
+    id_user = 552934290
+    album = 'profile'
+    count = 3
+    folder_parent = '17kd8jJyLUZuNi2Br4TXWyxarkXZdjFiX'
+    key = "river-runner-283818-aa3dd09a7020.json"
+
+    # id_user = input('Введите id пользователя: ')
+    # album = input('Из какого альбома нужно сохранить фотографии (wall/profile) : ')
+    # count = input('Сколько фотографий сохранить: ')
+    # token_vk = input('Введите токен для VK api: ')
     disk = input('На какой диск копировать (Google, Yandex - G/Y/G+Y) : ')
 
-    User = UserVK(id_user)
-    User.get_photo_album(album, count)
+    # User = UserVK(id_user)
+    # User.get_photo_album(album, count)
+
     if disk == 'G':
-        folder_parent = input('Введите id корневой папки Google.Диска: ')
-        key = input('Введите название ключа для сервисного аккаунта в формате JSON для Google.Диска: ')
+        # folder_parent = input('Введите id корневой папки Google.Диска: ')
+        # key = input('Введите название ключа для сервисного аккаунта в формате JSON для Google.Диска: ')
+        User = GoogleAPIClient(id_user)
+        User.get_photo_album(album, count)
         User.upload_photo_Google(folder_parent, key)
     elif disk == 'Y':
-        token_ya = input('Введите токен с Полигона Яндекс.Диска: ')
+        # token_ya = input('Введите токен с Полигона Яндекс.Диска: ')
+        User = YandexAPIClient(id_user)
+        User.get_photo_album(album, count)
         User.upload_photo_Yandex(token_ya)
     elif disk == 'G+Y':
-        folder_parent = input('Введите id корневой папки Google.Диска: ')
-        key = input('Введите название ключа для сервисного аккаунта в формате JSON для Google.Диска: ')
-        User.upload_photo_Google(folder_parent, key)
-        token_ya = input('Введите токен с Полигона Яндекс.Диска: ')
-        User.upload_photo_Yandex(token_ya)
+        # folder_parent = input('Введите id корневой папки Google.Диска: ')
+        # key = input('Введите название ключа для сервисного аккаунта в формате JSON для Google.Диска: ')
+        UserGoogle = GoogleAPIClient(id_user)
+        UserGoogle.get_photo_album(album, count)
+        UserGoogle.upload_photo_Google(folder_parent, key)
+        # token_ya = input('Введите токен с Полигона Яндекс.Диска: ')
+        UserYandex = YandexAPIClient(id_user)
+        UserYandex.photo_base = UserGoogle.photo_base
+        # UserYandex.get_photo_album(album, count)
+        UserYandex.upload_photo_Yandex(token_ya)
     else:
         print('некорректно указан диск')
