e80c2907da441e6bb3365c2f6c2181e3eac2846b
maxtv1982 <maxtv@mail.ru>
2020-08-29 14:14:09 +0400

    edit run

--- HomeWork/BD_ORM/run.py
+++ HomeWork/BD_ORM/run.py
@@ -11,40 +11,45 @@ Session.configure(bind=engine)
 
 if __name__ == '__main__':
     session = Session()
-    with open('tests_data.json', encoding="utf-8") as f:
-        json_data = json.load(f)
-    for item in json_data:
-        table = item["model"].capitalize()
-        if table == 'Publisher':
-            info = Publisher(id=item["pk"])
-            print(1)
-            session.add(info)
-            session.query(Publisher).filter(Publisher.id == item["pk"]).update(item["fields"])
-            session.commit()
-        elif table == 'Shop':
-            info = Shop(id=item["pk"])
-            print(2)
-            session.add(info)
-            session.query(Shop).filter(Shop.id == item["pk"]).update(item["fields"])
-            session.commit()
-        elif table == 'Book':
-            info = Book(id=item["pk"])
-            print(3)
-            session.add(info)
-            session.query(Book).filter(Book.id == item["pk"]).update(item["fields"])
-            session.commit()
-        elif table == 'Stock':
-            info = Stock(id=item["pk"])
-            print(4)
-            session.add(info)
-            session.query(Stock).filter(Stock.id == item["pk"]).update(item["fields"])
-            session.commit()
-        elif table == 'Sale':
-            info = Sale(id=item["pk"])
-            print(5)
-            session.add(info)
-            session.query(Sale).filter(Sale.id == item["pk"]).update(item["fields"])
-            session.commit()
 
 
+    def import_data(file):
+        with open(file, encoding="utf-8") as f:
+            json_data = json.load(f)
+        for item in json_data:
+            table = item["model"].capitalize()
+            if table == 'Publisher':
+                info = Publisher(id=item["pk"])
+                session.add(info)
+                session.query(Publisher).filter(Publisher.id == item["pk"]).update(item["fields"])
+                session.commit()
+            elif table == 'Shop':
+                info = Shop(id=item["pk"])
+                session.add(info)
+                session.query(Shop).filter(Shop.id == item["pk"]).update(item["fields"])
+                session.commit()
+            elif table == 'Book':
+                info = Book(id=item["pk"])
+                print(3)
+                session.add(info)
+                session.query(Book).filter(Book.id == item["pk"]).update(item["fields"])
+                session.commit()
+            elif table == 'Stock':
+                info = Stock(id=item["pk"])
+                session.add(info)
+                session.query(Stock).filter(Stock.id == item["pk"]).update(item["fields"])
+                session.commit()
+            elif table == 'Sale':
+                info = Sale(id=item["pk"])
+                session.add(info)
+                session.query(Sale).filter(Sale.id == item["pk"]).update(item["fields"])
+                session.commit()
+
+
+    # import_data('tests_data.json')
+    publisher_info = input('введите имя издателя: ')
+    shops = session.query(Shop).join(Stock, Stock.id_shop == Shop.id).join(Book, Book.id == Stock.id_book)\
+        .join(Publisher, Publisher.id == Book.id_publisher).filter(Publisher.name == publisher_info).all()
+    for shop in shops:
+         print(shop.name)
 
